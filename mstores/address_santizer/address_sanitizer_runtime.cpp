ade787a7488d3fec1b82cac98379dad8a556e9d5
baraveh <baraveh@gmail.com>
2020-11-05 10:42:08 +0200

    Initial shell

---
+++ mstores/address_santizer/address_sanitizer_runtime.cpp
@@ -0,0 +1,5 @@
+//
+// Created by baraveh on 11/5/20.
+//
+
+#include "address_sanitizer_runtime.h"
---
+++ mstores/address_santizer/address_sanitizer_runtime.h
@@ -0,0 +1,22 @@
+//
+// Created by baraveh on 11/5/20.
+//
+
+#ifndef COSMIX_ADDRESS_SANITIZER_H
+#define COSMIX_ADDRESS_SANITIZER_H
+
+#ifdef __cplusplus
+#include <cstdlib>
+#else
+#include <stdlib.h>
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif //COSMIX_ADDRESS_SANITIZER_H
