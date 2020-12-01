7dc3dbe5bc5f0446c33566f96fcec7efbae35713
baraveh <baraveh@gmail.com>
2020-12-01 15:34:55 +0200

    modified prints

--- mstores/address_sanitizer/address_sanitizer_runtime.cpp
+++ mstores/address_sanitizer/address_sanitizer_runtime.cpp
@@ -80,7 +80,7 @@ std::pair<bool, byte*> is_allowed(void* ptr, size_t size){
 void assert_access(void* ptr, size_t s){
     std::pair<bool, byte*> result = is_allowed(ptr, s);
     if(!result.first){
-        std::cerr << "Illegal access in address " << result.second << std::endl;
+        printf("Illegal access in address %p", result.second);
         exit(3);
     }
 }
