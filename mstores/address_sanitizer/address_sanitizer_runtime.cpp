e122abd735b26a9c2f5127f8e8c0106cb93506c3
baraveh <baraveh@gmail.com>
2020-12-01 17:29:09 +0200

    added more prints

--- mstores/address_sanitizer/address_sanitizer_runtime.cpp
+++ mstores/address_sanitizer/address_sanitizer_runtime.cpp
@@ -7,7 +7,9 @@
 #define SCALE (1<<SCALE_BITS)
 #define REDZONE_BYTES (SCALE) //must be scale aligned
 #define MEMORY_SIZE (1<<30)
-
+#define DEBUG 1
+#define debug_print(fmt, ...) \
+            do { if (DEBUG) fprintf(stderr, fmt, __VA_ARGS__); } while (0)
 
 #include "address_sanitizer_runtime.h"
 #include <sys/mman.h>
@@ -54,7 +56,7 @@ std::pair<bool, byte*> is_allowed(void* ptr, size_t size){
     // if pointer is not scale-aligned, special handling of the first byte
     if((uintptr_t)curr_byte % SCALE){
         curr_shadow_byte = get_shadow_byte(curr_byte);
-        printf("checking permissions for %p, shadow byte is %d\n", curr_byte, *curr_shadow_byte);
+        debug_print("checking permissions for %p, shadow byte is %d\n", curr_byte, *curr_shadow_byte);
         if (*curr_shadow_byte != SCALE){
             return std::pair<bool, byte*>(false, curr_byte);
         }
@@ -64,7 +66,7 @@ std::pair<bool, byte*> is_allowed(void* ptr, size_t size){
 
     while(remaining_bytes > 0){
         curr_shadow_byte = get_shadow_byte(curr_byte);
-        printf("checking permissions for %p, shadow byte is %d\n", curr_byte, *curr_shadow_byte);
+        debug_print("checking permissions for %p, shadow byte is %d\n", curr_byte, *curr_shadow_byte);
         if(remaining_bytes < SCALE){
             //the last byte, and size is not scale aligned
             return std::pair<bool, byte*>(*curr_shadow_byte >= remaining_bytes, curr_byte);
@@ -82,43 +84,43 @@ std::pair<bool, byte*> is_allowed(void* ptr, size_t size){
 void assert_access(void* ptr, size_t s){
     std::pair<bool, byte*> result = is_allowed(ptr, s);
     if(!result.first){
-        printf("Illegal access in address %p\n", result.second);
+        debug_print("Illegal access in address %p\n", result.second);
         exit(3);
     }
 }
 
 void mark_as_redzone(void* red_zone_ptr){
-    printf("marking %d bytes as redzone, starting from address %p", REDZONE_BYTES, red_zone_ptr);
+    debug_print("marking %d bytes as redzone, starting from address %p\n", REDZONE_BYTES, red_zone_ptr);
     for(int scale_byte_chunk = 0; scale_byte_chunk < REDZONE_BYTES/SCALE; scale_byte_chunk++){
-        printf("marking %p as redzone\n", (byte*)red_zone_ptr+SCALE*scale_byte_chunk);
+        debug_print("marking %p as redzone\n", (byte*)red_zone_ptr+SCALE*scale_byte_chunk);
         byte* shadow_byte = get_shadow_byte((byte*)red_zone_ptr+SCALE*scale_byte_chunk);
         *shadow_byte = -1;
     }
 }
 
 void mark_as_allocated(void* ptr, size_t size){
-    printf("marking %lu bytes as allocated, starting from address %p", size, ptr);
+    debug_print("marking %lu bytes as allocated, starting from address %p\n", size, ptr);
     for(int scale_byte_chunk = 0; scale_byte_chunk < size/SCALE; scale_byte_chunk++){
-        printf("marking %p as allocated\n", (((byte*)ptr)+SCALE*scale_byte_chunk));
+        debug_print("marking %p as allocated\n", (((byte*)ptr)+SCALE*scale_byte_chunk));
         byte* shadow_byte = get_shadow_byte((((byte*)ptr)+SCALE*scale_byte_chunk));
         *shadow_byte = SCALE;
     }
     if(size%SCALE > 0){ //size isn't divisible by scale, need to mark the shadow mem for the last k (1 ≤ k ≤ SCALE - 1) bytes
-        printf("marking %p as allocated\n", (((byte*)ptr)+(size-(size%SCALE))));
+        debug_print("marking %p as allocated\n", (((byte*)ptr)+(size-(size%SCALE))));
         byte* shadow_byte = get_shadow_byte((((byte*)ptr)+(size-(size%SCALE))));
         *shadow_byte = size%SCALE;
     }
 }
 
 void mark_as_freed(void* ptr, size_t size){
-    printf("marking %lu bytes as freed, starting from address %p", size, ptr);
+    debug_print("marking %lu bytes as freed, starting from address %p\n", size, ptr);
     for(int scale_byte_chunk = 0; scale_byte_chunk < size/SCALE; scale_byte_chunk++){
-        printf("marking %p as freed\n", (((byte*)ptr)+SCALE*scale_byte_chunk));
+        debug_print("marking %p as freed\n", (((byte*)ptr)+SCALE*scale_byte_chunk));
         byte* shadow_byte = get_shadow_byte((((byte*)ptr)+SCALE*scale_byte_chunk));
         *shadow_byte = 0;
     }
     if(size%SCALE > 0){ //size isn't divisible by eight, need to mark the shadow mem for the last k (1 ≤ k ≤ SCALE - 1) bytes
-        printf("marking %p as freed\n", (((byte*)ptr)+(size-(size%SCALE))));
+        debug_print("marking %p as freed\n", (((byte*)ptr)+(size-(size%SCALE))));
         byte* shadow_byte = get_shadow_byte((((byte*)ptr)+(size-(size%SCALE))));
         *shadow_byte = 0;
     }
@@ -140,8 +142,8 @@ int address_sanitizer_mstore_init(void *priv_data) {
     g_shadow_mem_size = (mem_limit.rlim_cur)/SCALE;
     g_shadow_mem = (byte*) mmap(nullptr, g_shadow_mem_size, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
     if(g_shadow_mem == MAP_FAILED){
-        printf("memory size is %lu, shadow mem size is %lu\n", mem_limit.rlim_cur, g_shadow_mem_size);
-        printf("mmap failed with %d - %s\n", errno, strerror(errno));
+        debug_print("memory size is %lu, shadow mem size is %lu\n", mem_limit.rlim_cur, g_shadow_mem_size);
+        debug_print("mmap failed with %d - %s\n", errno, strerror(errno));
         return -errno;
     }
     return 0;
@@ -167,7 +169,7 @@ void address_sanitizer_write_back(void *ptr, void *dst, size_t s){
 }
 
 void *address_sanitizer_mstore_alloc(size_t size, void *private_data){
-    printf("allocating %lu bytes, + %d redzone bytes\n", round_up_to_scale_aligned(size), 2*REDZONE_BYTES);
+    debug_print("allocating %lu bytes, + %d redzone bytes\n", round_up_to_scale_aligned(size), 2*REDZONE_BYTES);
     byte* ptr = (byte*) malloc(round_up_to_scale_aligned(size) + 2*REDZONE_BYTES);
     if(ptr == nullptr){
         return nullptr;
