216bb2ca6e78592a5fa7826ae446364f42316fd4
baraveh <baraveh@gmail.com>
2020-12-10 13:40:05 +0200

    beautified test

--- samples/heap_access/test.c
+++ samples/heap_access/test.c
@@ -17,7 +17,7 @@ extern void* __cosmix_address_sanitizer_annotation(void* ptr);
 void legal_heap_accesses();
 void left_heap_overflow();
 void right_heap_overflow();
-void access_after_free();
+void heap_access_after_free();
 
 struct test{
     char name[64];
@@ -26,65 +26,57 @@ struct test{
         {"Legal Heap Access", legal_heap_accesses},
         {"Left Heap Overflow", left_heap_overflow},
         {"Right Heap Overflow", right_heap_overflow},
-        {"Access After Free", access_after_free}
+        {"Heap Access After Free", heap_access_after_free}
 };
 
 
 void legal_heap_accesses(){
     char* x = (char*)__cosmix_address_sanitizer_annotation(malloc(sizeof(char)*8));
-    printf("Legal Heap Access - Allocated 8 bytes\n");
     x[7] = 'a';
     x[0] = 'b';
     x[3] = 'c';
     free(x);
-    printf("Legal Heap Access - Freed 8 bytes\n");
     char* y = (char*)__cosmix_address_sanitizer_annotation(malloc(sizeof(char)*13));
-    printf("Legal Heap Access - Allocated 13 bytes\n");
     for(int i = 0; i < 13; i++){
         y[i] = 'a';
     }
-
     int* z = (int*)__cosmix_address_sanitizer_annotation(malloc(sizeof(int)*13));
-    printf("Legal Heap Access - Allocated 13 bytes\n");
     z[12] = 1;
     z[11] = 1;
     z[0] = 1;
     free(z);
-    printf("Legal Heap Access - Freed 13 bytes\n");
     free(y);
-    printf("Legal Heap Access - Freed 13 bytes\n");
-    exit(3);
+    printf("Legal Heap Access - Passed\n");
+    exit(0);
 }
 
 void left_heap_overflow(){
     char* x = (char*)__cosmix_address_sanitizer_annotation(malloc(sizeof(char)*8));
     printf("Left Heap Overflow - Allocated 8 bytes from address %p\n",x);
-    printf("Left Heap Overflow - Trying to access %p\n",(x - 1));
+    printf("Left Heap Overflow - Trying to illegally access %p\n",(x - 1));
     x[-1] = 'a'; //should exit here
-    free(x);
-    exit(0);
+    assert(false);
 }
 
 void right_heap_overflow(){
     char* x = (char*)__cosmix_address_sanitizer_annotation(malloc(sizeof(char)*8));
     printf("Right Heap Overflow - Allocated 8 bytes from address %p\n", x);
-    printf("Right Heap Overflow - Trying to access %p\n", (x + 10));
+    printf("Right Heap Overflow - Trying to illegally access %p\n", (x + 10));
     x[10] = 'a'; //should exit here
-    free(x);
-    exit(0);
+    assert(false);
 }
 
-void access_after_free(){
+void heap_access_after_free(){
     char* x = (char*)__cosmix_address_sanitizer_annotation(malloc(sizeof(char)*8));
+    printf("Access After Free - Allocated 8 bytes from address %p\n", x);
     free(x);
     printf("Access After Free - Freed 8 bytes from address %p\n", x);
-    printf("Access After Free - Trying to access %p\n", (x + 1));
+    printf("Access After Free - Trying to illegally access %p\n", (x + 1));
     x[1] = 'a'; //should exit here
-    exit(0);
+    assert(false);
 }
 
-/** test pass ==> exit code = 3
- * When in doubt - change debug flag to 1 in address sanitizer runtime and check prints **/
+/** When in doubt - change debug flag to 1 in address sanitizer runtime and check prints **/
 int main(){
     for(int i = 0; i < sizeof(tests_arr) / sizeof(struct test); i++){
         pid_t pid = fork();
@@ -95,12 +87,7 @@ int main(){
         else{
             int status;
             waitpid(pid, &status, 0);
-            if (WIFEXITED(status) && WEXITSTATUS(status) == 3){
-                printf("%s Test Passed\n", tests_arr[i].name);
-            }
-            else{
-                printf("%s Test Failed\n", tests_arr[i].name);
-            }
+            printf("%s Test Finished\n", tests_arr[i].name);
         }
     }
 }
